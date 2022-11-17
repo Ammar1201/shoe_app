330a7b248c0711c74135475f1e2bbfeaba52541d
Ammar1201 <aamour1201@gmail.com>
2022-11-17 23:17:09 +0200

    added some style and show shoes data on shoes page

--- src/components/ShoeCard.js
+++ src/components/ShoeCard.js
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 import classes from './ShoeCard.module.css';
 
-const shoe = {
-  "name": "Adidas",
-  "picture": "https://images.unsplash.com/photo-1518002171953-a080ee817e1f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-  "price": "447.01",
-  "description": "Aspernatur assumenda quasi quos dolor ducimus quisquam. Pariatur minus placeat itaque et dolor.",
-  "id": "1"
-};
+// const shoe = {
+//   "name": "Adidas",
+//   "picture": "https://images.unsplash.com/photo-1518002171953-a080ee817e1f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+//   "price": "447.01",
+//   "description": "Aspernatur assumenda quasi quos dolor ducimus quisquam. Pariatur minus placeat itaque et dolor.",
+//   "id": "1"
+// };
 
-const ShoeCard = () => {
+const ShoeCard = ({shoe}) => {
   return ( 
     <div className={classes.card}>
       <h2>{shoe.name}</h2>
--- src/pages/Shoes.js
+++ src/pages/Shoes.js
@@ -1,10 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { getShoes } from "../api";
 import ShoeCard from "../components/ShoeCard";
+import classes from "./Shoes.module.css";
 
 const Shoes = () => {
+  const [shoes, setShoes] = useState([]);
+
+  useEffect(() => {
+    const data = async () => {
+      const shoesData = await getShoes();
+      console.log(shoesData);
+      setShoes(shoesData);
+    };
+    data();
+  }, []);
+
+
   return ( 
-    <div>
+    <div className={classes.container}>
       <h1>Shoes</h1>
-      <ShoeCard />
+      <div className={classes.shoesContainer}>
+        {shoes.map(shoe => <ShoeCard key={shoe.id} shoe={shoe} />)}
+      </div>
     </div> 
   );
 }
